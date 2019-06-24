778e8048a27019def8d8231b894c2a17ec4360c4
Kidim <mistter.kidim@gmail.com>
2019-06-24 15:10:57 +0000

    update code

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_network_security_group" "catapp-sg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"
-    source_address_prefix      = "*"
+    source_address_prefix      = "92.184.98.163"
     destination_address_prefix = "*"
   }
 
--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hashicorp-workshop"
+    organization = "Carrefour"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "serious"
     }
   }
 }
